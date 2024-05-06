e917928670c5e3c62421a81e84db9424dd341159
ixltech_swati <19329741-ixltech_swati@users.noreply.gitlab.com>
2024-05-06 12:01:33 +0530

    Single channel web socket connection R&D.

---
+++ routes/web.php
@@ -0,0 +1,15 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::post('/message-sent', function (\Illuminate\Http\Request $request){
+    if(!session()->has('username'))
+        session()->put('username', Faker\Factory::create()->userName);
+    \App\Events\MessageSent::dispatch(session()->get('username'), $request->message);
+    // \App\Events\MessageSent::dispatch('Swfsdfds', 'hello!');
+    return response()->json(['error'=> false, 'message'=> 'Message sent!']);
+});
